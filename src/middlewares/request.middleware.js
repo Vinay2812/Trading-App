29503aa8c1c358fefede10efbc826f09c69b0dcd
vinay2812 <vinaysarda2812@gmail.com>
2023-03-31 10:17:01 +0530

    ref: formatting

--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -1,17 +1,18 @@
-import createError from "http-errors"
+import createError from "http-errors";
+
 export default function ValidateRequest(validator = null) {
-    return function (req, res, next) {
-      if (null) {
-        return next();
-      }
-      const { error, value } = validator.validate(
-        { ...req.body, ...req.params, ...req.query, ...req.headers },
-        { allowUnknown: true, abortEarly: false }
-      );
-      if (error) {
-        return next(createError.UnprocessableEntity(error.details));
-      }
-      req.body = value;
-      next();
-    };
-  }
+  return function (req, res, next) {
+    if (null) {
+      return next();
+    }
+    const { error, value } = validator.validate(
+      { ...req.body, ...req.params, ...req.query, ...req.headers },
+      { allowUnknown: true, abortEarly: false }
+    );
+    if (error) {
+      return next(createError.UnprocessableEntity(error.details));
+    }
+    req.body = value;
+    next();
+  };
+}
